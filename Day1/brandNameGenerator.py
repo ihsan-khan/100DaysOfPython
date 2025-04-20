7b0d6b5cdd0d35540aab9b4a7f4c84d069bb479e
unknown <ehsankhan0577@gmail.com>
2025-04-20 18:48:58 +0500

    printing, string manipulation and varibales

---
+++ Day1/brandNameGenerator.py
@@ -0,0 +1,6 @@
+print('Welcome to the Brand Name Generator.')
+city = input('What is the name of the city you grew up in?\n')
+pet = input('What is the name of your pet?\n')
+brand_name = city + ' ' + pet
+print('Your brand name could be ' + brand_name + '!')
+# This is a simple program that generates a brand name based on the user's input.
